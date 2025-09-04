eba25d9a3087557d87c264d6e4e5c90b957d49b1
Diego Brito <dibbrito@gmail.com>
2025-09-04 19:59:22 -0300

    feat: add PATCH endpoint for updating todo completion status

--- main.go
+++ main.go
@@ -34,5 +34,19 @@ func main() {
 		return c.Status(201).JSON(todo)
 	})
 
+	app.Patch("/api/todos/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+		if err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid ID"})
+		}
+		for i, todo := range todos {
+			if todo.ID == id {
+				todos[i].Completed = !todos[i].Completed
+				return c.Status(200).JSON(todos[i])
+			}
+		}
+		return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	})
+
 	log.Fatal(app.Listen(":3000"))
 }
