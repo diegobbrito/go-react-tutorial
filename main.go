5acceda3b3a7f3346eb1ed55a3309de90a98f273
Diego Brito <dibbrito@gmail.com>
2025-09-08 20:13:07 -0300

    feat: implement updateTodo function to handle PATCH requests for todos

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodos)
+	app.Patch("/api/todos/:id", updateTodo)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -85,4 +86,24 @@ func createTodos(c *fiber.Ctx) error {
 	}
 	todo.ID = result.InsertedID.(primitive.ObjectID)
 	return c.Status(201).JSON(todo)
+}
+
+func updateTodo(c *fiber.Ctx) error {
+	id := c.Params("id")
+	objectID, err := primitive.ObjectIDFromHex(id)
+
+	if err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": "Invalid todo ID"})
+	}
+
+	filter := bson.M{"_id": objectID}
+	update := bson.M{"$set": bson.M{"completed": true}}
+
+	_, err = collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return c.Status(200).JSON(fiber.Map{"success": true})
+
 }
